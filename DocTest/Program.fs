2fc4a09d956fc6c845ddc3c51819dc873eb49fb5
Henry Henriques <hhenriques404@gmail.com>
2019-08-22 10:48:09 +0200

    Added dummy project and DocFx

---
+++ DocTest/Program.fs
@@ -0,0 +1,115 @@
+﻿namespace Doc
+/// <summary>
+/// The main <c>Math</c> class.
+/// Contains all methods for performing basic math functions.
+/// <list type="bullet">
+/// <item>
+/// <term>Add</term>
+/// <description>Addition Operation</description>
+/// </item>
+/// <item>
+/// <term>Subtract</term>
+/// <description>Subtraction Operation</description>
+/// </item>
+/// <item>
+/// <term>Multiply</term>
+/// <description>Multiplication Operation</description>
+/// </item>
+/// <item>
+/// <term>Divide</term>
+/// <description>Division Operation</description>
+/// </item>
+/// </list>
+/// </summary>
+/// <remarks>
+/// <para>This class can add, subtract, multiply and divide.</para>
+/// <para>These operations can be performed on both integers and doubles.</para>
+/// </remarks>
+module Math =
+    // Adds two integers and returns the result
+    /// <summary>
+    /// Adds two integers <paramref name="a"/> and <paramref name="b"/> and returns the result.
+    /// </summary>
+    /// <returns>
+    /// The sum of two integers.
+    /// </returns>
+    /// <example>
+    /// <code>
+    /// int c = Math.Add(4, 5);
+    /// if (c > 10)
+    /// {
+    ///     Console.WriteLine(c);
+    /// }
+    /// </code>
+    /// </example>
+    /// <param name="a">An integer.</param>
+    /// <param name="b">An integer.</param>
+    let Add a b =
+    // If any parameter is equal to the max value of an integer
+    // and the other is greater than zero
+        a + b
+
+
+    // Subtracts an integer from another and returns the result
+    /// <summary>
+    /// Subtracts <paramref name="b"/> from <paramref name="a"/> and returns the result.
+    /// </summary>
+    /// <returns>
+    /// The difference between two integers.
+    /// </returns>
+    /// <example>
+    /// <code>
+    /// int c = Math.Subtract(4, 5);
+    /// if (c > 1)
+    /// {
+    ///     Console.WriteLine(c);
+    /// }
+    /// </code>
+    /// </example>
+    /// <param name="a">An integer.</param>
+    /// <param name="b">An integer.</param>
+    let Subtract a b =
+        a - b;
+
+
+    // Multiplies two integers and returns the result
+    /// <summary>
+    /// Multiplies two integers <paramref name="a"/> and <paramref name="b"/> and returns the result.
+    /// </summary>
+    /// <returns>
+    /// The product of two integers.
+    /// </returns>
+    /// <example>
+    /// <code>
+    /// int c = Math.Multiply(4, 5);
+    /// if (c > 100)
+    /// {
+    ///     Console.WriteLine(c);
+    /// }
+    /// </code>
+    /// </example>
+    /// <param name="a">An integer.</param>
+    /// <param name="b">An integer.</param>
+    let Multiply a b =
+        a * b
+
+    // Divides an integer by another and returns the result
+    /// <summary>
+    /// Divides an integer <paramref name="a"/> by another integer <paramref name="b"/> and returns the result.
+    /// </summary>
+    /// <returns>
+    /// The quotient of two integers.
+    /// </returns>
+    /// <example>
+    /// <code>
+    /// int c = Math.Divide(4, 5);
+    /// if (c > 1)
+    /// {
+    ///     Console.WriteLine(c);
+    /// }
+    /// </code>
+    /// </example>
+    /// <param name="a">An integer dividend.</param>
+    /// <param name="b">An integer divisor.</param>
+    let Divide a b = 
+        a / b
